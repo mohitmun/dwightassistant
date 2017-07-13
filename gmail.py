d70bb9c30e129f16ee21ed50cb790397d9a62e05
Mohit M <mohitcrox@gmail.com>
2017-07-13 21:39:03 +0530

    gmail

---
+++ gmail.py
@@ -0,0 +1,69 @@
+def get_bot():
+  return {"name":'Dwight',
+        "description":'An Assistant to you',
+        "intents":[
+            {
+                'intentName': 'GetLastMail',
+                'intentVersion': '1.0'
+            },
+        ],
+        "clarificationPrompt":{
+            'messages': [
+                {
+                    'contentType': 'PlainText',
+                    'content': 'Welcome to Dwight. Type "Show me my last email"'
+                },
+            ],
+            'maxAttempts': 5,
+            'responseCard': 'string'
+        },
+        "abortStatement":{
+            'messages': [
+                {
+                    'contentType': 'PlainText',
+                    'content': 'Bot failed'
+                },
+            ],
+            'responseCard': 'string'
+        },
+        "idleSessionTTLInSeconds":123,
+        "locale":'en-US',
+        "childDirected":True}
+def get_slots():
+  return [
+    {
+      'name': "From"
+    }
+  ]
+def get_intents():
+  return [
+    {
+      "name": "GetLastMail",
+      "description": "Get last email from you gmail inbox",
+      "sampleUtterances": ["Show me last email", "Get me last email", "whats my last email"],
+      # "dialogCodeHook": {
+      #   "uri": "test",
+      #   "messageVersion": "$LATEST"
+      # }
+      "fulfillmentActivity": {
+        "type": "CodeHook",
+        "codeHook": {
+          "uri": "arn:aws:lambda:us-east-1:420758276632:function:test",
+          "messageVersion": "2.0"
+        }
+      },
+      "conclusionStatement": {
+        "messages": [
+          {
+            "contentType": "PlainText",
+            "content": "conclu 1"
+          },
+          {
+            "contentType": "PlainText",
+            "content": "conclu 2"
+          }
+        ]
+      }
+    }
+  ]
+  
