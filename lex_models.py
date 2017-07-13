5055268164ca8379b5a113dc6eb6bd26e733c353
Mohit M <mohitcrox@gmail.com>
2017-07-13 23:57:44 +0530

    mc

--- lex_models.py
+++ lex_models.py
@@ -1,43 +1,40 @@
 import boto3
-
+import gmail
 class LexApi:
-  
   def __init__(self):
     self.client = boto3.client('lex-models')
-  def create_dwight(self):
-    response = self.client.put_bot(
-      name='Dwight',
-      description='An Assistant to you',
-      # intents=[
-      #     {
-      #         'intentName': 'getLastEmail',
-      #         'intentVersion': '$LATEST'
-      #     },
-      # ],
-      clarificationPrompt={
-          'messages': [
-              {
-                  'contentType': 'PlainText',
-                  'content': 'Welcome to Dwight. Type "Show me my last email"'
-              },
-          ],
-          'maxAttempts': 5,
-          'responseCard': 'string'
-      },
-      abortStatement={
-          'messages': [
-              {
-                  'contentType': 'PlainText',
-                  'content': 'Bot failed'
-              },
-          ],
-          'responseCard': 'string'
-      },
-      idleSessionTTLInSeconds=123,
-      locale='en-US',
-      childDirected=True|False
-    )
+  def delete_bot(self):
+    response = self.client.delete_bot(name="Dwight")
+    return response
+  def delete_intent(self, name):
+    response = self.client.delete_intent(name=name)
+    return response
+  def create_bot(self):
+    response = self.client.put_bot(**gmail.get_bot())
+    return response
   def create_intent(self):
-    response = self.client.put_intent(
-      name="GetLastMail"
-    )
+    res = []
+    for intent in gmail.get_intents():
+      response = self.client.put_intent(**intent)
+      res.append(response)
+    return res
+  def get_bots(self):
+    return self.client.get_bots()
+  # def set_up_bot():
+  #   pass
+
+class LexRunTimeApi:
+  """docstring for LexRunTimeApi"""
+  def __init__(self):
+    self.client = boto3.client('lex-runtime')
+  def send_message(self, message):
+#     client.post_text(
+#     botName='string',
+#     botAlias='string',
+#     userId='string',
+#     sessionAttributes={
+#         'string': 'string'
+#     },
+#     inputText='string'
+# )
+    return self.client.post_text(botName="Dwight", botAlias="$LATEST", userId="mama", inputText=message)
