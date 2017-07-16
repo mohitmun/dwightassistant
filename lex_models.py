4b9442804b57925b4273a0e16bb83413bb2afdd6
Mohit M <mohitcrox@gmail.com>
2017-07-16 15:55:13 +0530

    mc

--- lex_models.py
+++ lex_models.py
@@ -6,15 +6,20 @@ class LexApi:
   def delete_bot(self):
     response = self.client.delete_bot(name="Dwight")
     return response
-  def delete_intent(self, name):
-    response = self.client.delete_intent(name=name)
-    return response
+  def delete_intent(self):
+    self.delete_bot();
+    for intent in gmail.get_intents():    
+      response = self.client.delete_intent(name=intent['name'])
+    return True
   def create_bot(self):
-    response = self.client.put_bot(**gmail.get_bot())
+    params = gmail.get_bot()
+    params["checksum"] = self.get_bot()["checksum"]
+    response = self.client.put_bot(**params)
     return response
   def create_intent(self):
     res = []
     for intent in gmail.get_intents():
+      intent["checksum"] = self.get_intent(intent["name"])["checksum"]
       response = self.client.put_intent(**intent)
       res.append(response)
     return res
@@ -22,8 +27,12 @@ class LexApi:
     return self.client.get_bots()
   # def set_up_bot():
   #   pass
+  def get_bot(self):
+    return self.client.get_bot(name="Dwight",versionOrAlias="$LATEST")
   def get_bot_channel_associations(self):
     return self.client.get_bot_channel_associations(botName="Dwight",botAlias="LATEST")
+  def get_intent(self, name):
+    return self.client.get_intent(name=name, version="$LATEST")
 
 class LexRunTimeApi:
   """docstring for LexRunTimeApi"""
