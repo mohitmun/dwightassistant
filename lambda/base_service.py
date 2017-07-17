8f9978e100058072a74a399c6dd21b2192e38e40
Mohit M <mohitcrox@gmail.com>
2017-07-17 17:44:41 +0530

    curl wrapper yeah bitch

--- lambda/base_service.py
+++ lambda/base_service.py
@@ -2,7 +2,7 @@ import utils
 import dynamodb
 import os
 import json
-
+import dotmap
 class BaseService:
   
   def __init__(self, service1):
@@ -39,6 +39,7 @@ class BaseService:
   def get_and_save_access_code(self, user_id, command):
     response = os.popen(command).read()
     # response = json.loads(response)
+    print(response)
     dynamodb.update_user(user_id, self.auth_key(), response)
     response = json.loads(response)
     dynamodb.update_user(user_id, self.access_token_key(), response["access_token"])
@@ -46,3 +47,14 @@ class BaseService:
 
   def send_api_auth_link(self, user_id):
     return utils.send_message("Please give access to you {0} account {1}?user_id={2}".format(self.service, utils.get_api_auth_url("connect-{0}".format(self.service)), user_id))
+
+  def authorized_curl(self, s, user):
+    return self.curl("{0} -H 'Authorization: Bearer {1}'".format(s, self.get_access_token(user)))
+
+  def curl(self, s):
+    cmd = "curl {0}".format(s)
+    print(cmd)
+    res = os.popen(cmd).read()
+    res = json.loads(res)
+    res = dotmap.DotMap(res)
+    return res
