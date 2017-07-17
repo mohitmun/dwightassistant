107b394816a2f2be05207c0aa1a83c746540516e
Mohit M <mohitcrox@gmail.com>
2017-07-18 00:52:53 +0530

    one click setup WIP

--- lambda_api.py
+++ lambda_api.py
@@ -2,6 +2,7 @@ import os
 import zipfile
 import boto3
 import env
+import time
 class LambdaApi:
   def __init__(self):
     self.client = boto3.client('lambda')
@@ -9,7 +10,7 @@ class LambdaApi:
   def list_functions(self):
     return self.client.list_functions()
   
-  def update_function_code(self):
+  def update_function_code(self, FunctionName):
     # zip_file = zipfile.ZipFile("lambda.zip", "w")  
     # zip_file.write("lambdama/gmail.py",compress_type=zipfile.ZIP_DEFLATED)
     with zipfile.ZipFile('lambda.zip', 'w') as myzip:
@@ -20,18 +21,18 @@ class LambdaApi:
           myzip.write(os.path.join(root, f), os.path.join(root, f)[7:])
     # return 1
     print("Uploading started")
-    res = self.client.update_function_code(FunctionName="test", ZipFile=open("lambda.zip", "rb").read())
+    res = self.client.update_function_code(FunctionName=FunctionName, ZipFile=open("lambda.zip", "rb").read())
     try:
       os.system('osascript -e \'display notification "Uplading done" with title "ApiGateway"\'')
     except Exception as e:
       print("error")
     return res
   
-  def add_permission(self):
-    return self.client.add_permission(FunctionName="test",StatementId="ID-12chutest", Action= "lambda:*", Principal="lex.amazonaws.com")
+  def add_permission(self, FunctionName, Action, Principal):
+    return self.client.add_permission(FunctionName=FunctionName,StatementId="ID-{0}-{1}".format(FunctionName, int(time.time())), Action= Action, Principal=Principal)
 
-  def update_function_configuration(self):
-    return self.client.update_function_configuration(FunctionName= "test", Environment={'Variables': env.get_env()})
+  def update_function_configuration(self, FunctionName):
+    return self.client.update_function_configuration(FunctionName= FunctionName, Environment={'Variables': env.get_env()})
     #     FunctionName='string',
     # StatementId='string',
     # Action='string',
