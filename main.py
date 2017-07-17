7d6d828dec3662b25b9f7079af2cfa1ce004d355
Mohit M <mohitcrox@gmail.com>
2017-07-17 15:14:52 +0530

    mc

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import lex_models
 import lambda_api
+import api_gateway
 print("chus1")
 api_lex = lex_models.LexApi()
 api_lambda = lambda_api.LambdaApi()
 api_runtime = lex_models.LexRunTimeApi()
-
+api_gateway = api_gateway.ApiGateway()
 
 # api_lex.create_intent()
 # api_lex.create_bot()
