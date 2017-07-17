624002e56caa481bf3e42b32af0c672ea85b6781
Mohit M <mohitcrox@gmail.com>
2017-07-17 18:21:47 +0530

    mc

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -16,12 +16,16 @@ def lambda_handler(event, context):
     if event["resource"] == "/connect-spotify":
       return spotify.redirect_to_auth(event)
 
+    if event["resource"] == "/gmail":
+      return gmail.save_access_token(event)
+    if event["resource"] == "/connect-gmail":
+      return gmail.redirect_to_auth(event)
 
 def test():
   event = {
   "currentIntent": {
     "slots": {},
-    "name": "ConnectSpotify",
+    "name": "PlaySpotify",
     "confirmationStatus": "None"
   },
   "bot": {
@@ -41,7 +45,7 @@ def test():
 def test_spo():
   event = {
   "body": "null",
-  "resource": "/connect-spotify",
+  "resource": "/spotify",
   "requestContext": {
     "resourceId": "r1iee2",
     "apiId": "tn78yzlfic",
@@ -66,10 +70,13 @@ def test_spo():
     },
     "stage": "test-invoke-stage"
   },
-  "queryStringParameters": {"code": "chus", "user_id": "123"},
+  "queryStringParameters": {"code": "AQB71S_brJ0i-HbiFiJr4N5RMCtTcU1fGo-YqUQapJ9WuPDyxI-ICHZtOzOTDlEre1zWjkOrmgrSuoAYp4SSKZGUAlQ7o3Za8F56X4OddiqFvAyg-SswbUYdJIZ66QXZvEpU3pflUXmI3NHB-w84qs_IynPTw0eWwJ8B3PlDJDkImGXk10LyA5ckvX6-n6Po_Hxj2-k8Qx_dx0Eepf2MHeEMMuvsCoMCYf2CtsVEzISqUf-VvsosnrqMA2ZCMAbMHECTzl7bLucsBUAiJGtaN53n2kYL2BA-dQem0ezLqw9lBHY3scSWNxgUbOuyeL1d2M6UkPpt7F8t13oLn4Ds9lIXcMRw86KMwoh9FI5gFeJji3drcCP1u2ylFAlr27oKRstyouejaDNEpY372tuEpJgBuw-tS7FjZYVbR-Sr--5NqkljhrecMjQYHTP6BzQWKKMA4lSM65SS_xYRV7ASmSoAB-qfyIKc_pgGGjD5qpWUBX239g88MFBvQIk6hdfM_XDgXSEE3eMb749vUoUN6nMo7VyPu7nnciNZwJoT1ZTVPK6E9zfJGsVsGGDwyiHnlXBjlwTUEtonn7xy9mt7L09HWn8Lt0UjHME_JYJA1hpI8z21O-Sz7pQdHh1JdB2QFMOB1fW9v7o0mLORepDcFVjGpNXkEfXCbw4mXEpYZLGc6omgvrSXmhCpbJebNBDyXpi8Fyywi7WP6UKLbD_M", "user_id": "123"},
   "httpMethod": "GET",
   "pathParameters": "null",
-  "headers": "null",
+  # "headers": "null",
+  "headers": {
+   "Cookie": "user_id=ZAKQI3"
+  },
   "stageVariables": "null",
   "path": "/spotify",
   "isBase64Encoded": "false"
