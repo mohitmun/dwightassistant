121da8ff26bc641cc4d1f9d99b860f7dc0be1c82
Mohit M <mohitcrox@gmail.com>
2017-07-18 16:20:01 +0530

    spotify almost working

--- lambda/spotify.py
+++ lambda/spotify.py
@@ -51,28 +51,110 @@ def handle(event):
     if base_service.token_expired(user):
       user = refresh_access_token(user)
     if underscore_name == "play_spotify":
-      return play_spotify(user, event)
+      return play_spotify_intent(user, event)
     if underscore_name == "stop_spotify":
-      return stop_spotify(user, event)
+      return stop_spotify_intent(user, event)
   else:
     return base_service.send_api_auth_link(user["user_id"]["S"])
 
 def token_expired(user):
   return base_service.token_expired(user)
 
-def play_spotify(user, event):
-  command = "curl -X PUT 'https://api.spotify.com/v1/me/player/play' -H 'Authorization: Bearer {0}'".format(base_service.get_access_token(user))
-  print(command)
-  return utils.send_message(os.popen(command).read() + " event: " + json.dumps(event))
+# event = {"currentIntent": {"slots": {"album": "", "song": "", "artist": ""}}}
+def play_spotify_intent(user, event):
+  slots = event["currentIntent"]["slots"]
+  song = slots["song"]
+  album = slots["album"]
+  artist = slots["artist"]
+  if song != None:
+    if artist != None:
+      searched_song = search_song_by_artist(user, song, artist)
+    else:  
+      searched_song = search_song(user, song)
+    if searched_song:
+      return play_tracks_intent(user, [searched_song["uri"]])
+    else:
+      return utils.send_message("No results found!")
+  elif album != None:
+    searched_album = search_album(user, album)
+    if searched_album:
+      return play_context_intent(user, searched_album["uri"])
+    else:
+      return utils.send_message("No results found!")
+  elif artist != None:
+    searched_artist = search_artist(user,artist)
+    if searched_artist:
+      return play_context_intent(user, searched_artist["uri"])
+    else:
+      return utils.send_message("No results found!")
+  command = "-X PUT 'https://api.spotify.com/v1/me/player/play'"
+  res = base_service.authorized_curl(command)
+  return utils.send_message(json.dumps(res) + " event: " + json.dumps(event))
 
-def stop_spotify(user, event):
-  command = "curl -X PUT 'https://api.spotify.com/v1/me/player/pause' -H 'Authorization: Bearer {0}'".format(base_service.get_access_token(user))
-  print(command)
-  return utils.send_message(os.popen(command).read() + " event: " + json.dumps(event))
+def stop_spotify_intent(user, event):
+  command = "-X PUT 'https://api.spotify.com/v1/me/player/play'"
+  res = base_service.authorized_curl(command, user)
+  return utils.send_message(json.dumps(res) + " event: " + json.dumps(event))
 
 def test_user():
   return dynamodb.get_item("XB29T8")
 
+def search_song_by_artist(user, track_q, artist_q):
+  items = search_item(user, "track:{}%20artist:{}".format(track_q, artist_q), "track")["tracks"]["items"]
+  if len(items) > 0:
+    return items[0]
+  else:
+    return None
+
+def play_tracks_intent(user, uris):
+  data = {"uris": uris}
+  command = "-X PUT 'https://api.spotify.com/v1/me/player/play' -H 'Content-Type: application/json' --data '{}'".format(json.dumps(data))
+  res = base_service.authorized_curl(command, user)
+  return utils.send_message("played track: " + json.dumps(uris) + " res: " + json.dumps(res))
+
+def play_context_intent(user, context_uri):
+  data = {"context_uri": context_uri}
+  command = "-X PUT 'https://api.spotify.com/v1/me/player/play' -H 'Content-Type: application/json' --data '{}'".format(json.dumps(data))
+  res = base_service.authorized_curl(command, user)
+  return utils.send_message("playing context: " + json.dumps(context_uri) + " res: " + json.dumps(res))
+
+def search_album(user, q):
+  res = search_item(user, q, "album")
+  items = res["albums"]["items"]
+  if len(items) > 0:
+    return items[0]
+  else:
+    return None
+
+def search_artist(user, q):
+  res = search_item(user, q, "artist")
+  items = res["artists"]["items"]
+  if len(items) > 0:
+    return items[0]
+  else:
+    return None
+
+def search_song(user, q):
+  res = search_item(user, q, "track")
+  items = res["tracks"]["items"]
+  if len(items) > 0:
+    return items[0]
+  else:
+    return None
+
+def get_album_songs(album_id, user):
+  command = "-X GET https://api.spotify.com/v1/albums/{}/tracks".format(album_id)
+  res = base_service.authorized_curl(command, user)
+  return res
+
+def search_item(user, q, q_type):
+  # Valid types are: album, artist, playlist, and track.
+  q = q.replace(" ", "%20")
+  q_type = q_type.replace(" ", "+")
+  url = "https://api.spotify.com/v1/search?q={}&type={}&limit=10".format(q, q_type)
+  command = "-X GET '{}'".format(url)
+  res = base_service.authorized_curl(command, user)
+  return res
 # curl -X PUT "https://api.spotify.com/v1/me/player/play" -H "Authorization: Bearer BQCF5bnU0EY0uTpkoC3HUl-66YJjZXu5ULt503BHEP-9WkMcro2xJcO4atyxl04hIdW4z_aLdHwslDX40oJSAsmX2h6e99Dvv4EOAl7Xj4dM_utbPJf9Adk0fuD0yas_vfPTjpjgfdmQYkE"
 
 # https://accounts.spotify.com/authorize/?client_id=a7ed7ac582c4421397e4336a9339d849&response_type=code&redirect_uri=https%3A%2F%2Fmamam.com&scope=user-modify-playback-state
