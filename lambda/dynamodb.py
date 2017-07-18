a15760a95dbaf8a44da319569f0e862ff4c723d9
Mohit M <mohitcrox@gmail.com>
2017-07-18 23:21:49 +0530

    botname==tablename

--- lambda/dynamodb.py
+++ lambda/dynamodb.py
@@ -3,19 +3,20 @@ import string
 import random
 client = boto3.client('dynamodb')
 
+TableName = os.environ["BOT_NAME"]
 def add_user(lex_user_id):
   random_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
-  res = client.put_item(TableName="users", Item={"user_id": {"S": random_id}, "lex_user_id": {"S": lex_user_id}})
+  res = client.put_item(TableName=TableName, Item={"user_id": {"S": random_id}, "lex_user_id": {"S": lex_user_id}})
   return get_item(random_id)
 
 def get_item(id):
-  return client.get_item(TableName="users", Key={"user_id": {"S": id}})["Item"]
+  return client.get_item(TableName=TableName, Key={"user_id": {"S": id}})["Item"]
 
 def update_user(user_id, key, value):
-  return client.update_item(TableName="users", Key={"user_id":{"S":user_id}}, AttributeUpdates={key:{"Value":{"S":value}}})
+  return client.update_item(TableName=TableName, Key={"user_id":{"S":user_id}}, AttributeUpdates={key:{"Value":{"S":value}}})
 
 def get_user(lex_user_id):
-  return client.scan(TableName="users", FilterExpression="lex_user_id = :a", ExpressionAttributeValues={
+  return client.scan(TableName=TableName, FilterExpression="lex_user_id = :a", ExpressionAttributeValues={
         ':a': {
             'S': lex_user_id,
         }})["Items"]
