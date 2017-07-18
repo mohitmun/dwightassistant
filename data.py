3de0177c2ff0b79be3b78ba296a93e96cf25e060
Mohit M <mohitcrox@gmail.com>
2017-07-18 09:14:38 +0530

    more organised much wow

--- data.py
+++ data.py
@@ -2,46 +2,20 @@ import yaml
 def get_bot():
   intents = []
   for i in get_intents():
-    intents.append(
-            {
-                'intentName': i["name"],
-                'intentVersion': '$LATEST'
-            })
-  return {"name":'Dwight',
-        "description":'An Assistant to you',
-        "intents": intents,
-        "clarificationPrompt":{
-            'messages': [
-                {
-                    'contentType': 'PlainText',
-                    'content': 'Welcome to Dwight. Type "Show me my last email"'
-                },
-            ],
-            'maxAttempts': 5,
-            'responseCard': 'string'
-        },
-        "abortStatement":{
-            'messages': [
-                {
-                    'contentType': 'PlainText',
-                    'content': 'Bot failed'
-                },
-            ],
-            'responseCard': 'string'
-        },
-        "idleSessionTTLInSeconds":123,
-        "locale":'en-US',
-        "childDirected":True,
-        }
-def get_slots():
-  return [
-    {
-      'name': "From"
-    }
-  ]
+    intents.append({
+      'intentName': i["name"],
+      'intentVersion': '$LATEST'
+    })
+  with open("data.yml", 'r') as stream:
+    result = yaml.load(stream)["bot"]
+  result["intents"] = intents
+  return result
+
 def get_intents():
   with open("data.yml", 'r') as stream:
-    try:
-        return yaml.load(stream)["intents"]
-    except yaml.YAMLError as exc:
-        print(exc)
+    return yaml.load(stream)["intents"]
+
+def get_slot_types():
+  with open("data.yml", 'r') as stream:
+    return yaml.load(stream)["slot_types"]
+    
